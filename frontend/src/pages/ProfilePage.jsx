d12c5beca17d5e7def87d64757f1ab6ccf042e84
Nahuel Bogado <ultrashunior@gmail.com>
2023-09-26 15:44:57 -0300

    ready for deploy V1

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -4,9 +4,12 @@ function ProfilePage() {
   const { user } = useAuth();
 
   return (
-    <div>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold">Profile</h1>
+      <p className="my-4">Welcome, {user.name}!</p>
+      <p className="my-4">Email: {user.email}</p>
+      <p className="my-4">User create at: {user.created_at.slice(0,10)}</p>
+    </>
   );
 }
 
